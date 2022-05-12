0e53c3c2c27d9feb81328565d96b8fccefac2aaa
Xanax <Merwwj@gmail.com>
2022-05-12 12:38:16 +0430

    removed the  semicolon that made me lose couple of hair

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
   StyleSheet,
@@ -7,33 +7,49 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Keyboard,
 } from "react-native";
 import Task from "./components/Task";
-
-
 export default function App() {
-  const [task , setTask] = useState()
-  const newTaskHandler = () =>{
-    console.log(task)
-  }
-
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  // functions
+  const newTaskHandler = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+    console.log("new task handler fired");
+  };
+  const completedTasks = (i) => {
+    let itemCp = [...taskItems];
+    itemCp.splice(i, 1);
+    setTaskItems(itemCp);
+    console.log("completed tasks fired");
+  };
   return (
     <View style={styles.container}>
-      {/* Current Tasks */}
       <View style={styles.tasksContainer}>
         <Text style={styles.mainTitle}>Current Tasks</Text>
         <View style={styles.tasks}>
-          {/* where tasks will be */}
-          <Task text={"Task 1"} />
-          <Task text={"Task 2"} />
+          {taskItems.map((item, i) => {
+            return (
+              <TouchableOpacity key={i} onPress={() => completedTasks(i)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
-      {/* Create Tasks */}
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.createTaskContainer}
       >
-        <TextInput style={styles.input} placeholder={"New Task"} value={task} onChangeText={text => setTask(text)}/>
+        <TextInput
+          style={styles.input}
+          placeholder={"New Task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
         <TouchableOpacity onPress={() => newTaskHandler()}>
           <View style={styles.addContainer}>
             <Text style={styles.addText}>+</Text>
@@ -90,6 +106,6 @@ const styles = StyleSheet.create({
   },
   addText: {
     fontSize: 20,
-    color: '#0Aaccc'
+    color: "#0Aaccc",
   },
 });
